ef3b69213cea716e147e7f9995a5401b8a93ae12
Vladislav Zubakin <shaqimusic@gmail.com>
2022-06-23 02:01:53 +0300

    feat: add map rendering

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const form = document.querySelector("form")!;
+const mapEl = document.getElementById("map")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "AIzaSyBoHJLkL6YDzYCA8E4kIu2WMRBhL5tcj9M";
 
 type GoogleGeocodingResponse = {
 	results: { geometry: { location: { lat: number; lng: number } } }[];
-  status: 'OK' | 'ZERO_RESULTS'
+	status: "OK" | "ZERO_RESULTS";
 };
 
 function searchAddressHandler(event: Event) {
@@ -21,13 +22,22 @@ function searchAddressHandler(event: Event) {
 			)}&key=${GOOGLE_API_KEY}`
 		)
 		.then((response) => {
-      if(response.data.status !== 'OK') {
-        throw new Error('Could not fetch location');
-      }
+			if (response.data.status !== "OK") {
+				throw new Error("Could not fetch location");
+			}
 			const coordinates = response.data.results[0].geometry.location;
+			const map = new google.maps.Map(mapEl, {
+				center: coordinates,
+				zoom: 16,
+			});
+
+			new google.maps.Marker({
+				position: coordinates,
+				map: map,
+			});
 		})
 		.catch((error) => {
-      alert(error.message);
+			alert(error.message);
 			console.log(error);
 		});
 }
